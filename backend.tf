3af4b207b60976f078beb9561d247a87e64dd45a
Agus <a.calvimontes@reply.de>
2025-11-17 18:21:50 +0100

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,9 +1,6 @@
-# backend.tf - Partial Backend Configuration
-
 terraform {
   backend "s3" {
-    # Everyone uses the same bucket and lock table
-    bucket         = "tum-workshop-tfstate" # A single, shared S3 bucket
+    bucket         = "tum-workshop-tfstate" 
     region         = "eu-central-1"
     encrypt        = true
   }
